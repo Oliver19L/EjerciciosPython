527c68eb19833e0a04842f14397c2931ca37a0b0
Oliver-Luna <oliver21luna999@gmail.com>
2021-07-27 17:49:17 -0600

    subiendo el repositorio de python

---
+++ Ejercicios Principiantes/JugueteriaPeso.py
@@ -0,0 +1,14 @@
+toysM = 75
+ToysP = 112
+
+cantidadM = input(print("Cuantas Muñecas se vendieron: "))
+cantidadM = int(cantidadM)
+cantidadP = input(print("Cuantos Payasos se vendieron: "))
+cantidadP = int(cantidadP)
+
+PesoM = cantidadM*toysM
+PesoP = cantidadP*ToysP
+
+print("El peso total de las munetas son: " +str(PesoM))
+print("El peso total de los payasos es: " +str(PesoP))
+
